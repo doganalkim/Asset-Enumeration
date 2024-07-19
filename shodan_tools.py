ad3da51ebb8c19e777b4c319f71f5511f3ca49a6
Yasin Çelik <yasincelik518@gmail.com>
2024-07-19 10:13:11 +0300

    shodan_tools added

--- shodan_tools.py
+++ shodan_tools.py
@@ -19,6 +19,20 @@ def api(favhash=None,use_api_key=False, api_key=None):
     return result
 
 
+def sub_osint(key, domain):
+    SHODAN_API_KEY = key
+    subdomain = domain
+    api = shodan.Shodan(SHODAN_API_KEY)
+
+
+    fields = ["ip_str", "port", "hostnames", "org", "isp", "asn", "os", "location", "domains", "product", "version", "cpe"]
+    results = api.search('hostname:' + subdomain, fields=fields, minify=False)
+
+    return results
+
 if __name__ == "__main__":
-    x=url("https://python.org")
-    api(x,True,"YOUR_APİ_KEY")
+    fhash = url("https://python.org")
+    fresult = api(fhash,True,"YOUR_APİ_KEY")
+    sub_result = sub_osint('YOUR_APİ_KEY','DOMAİN')
+    print(fresult)
+    print(sub_result)
